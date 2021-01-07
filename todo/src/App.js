eed18140dd6b3410f6a6d8861a9a2b4fcffbb716
Angelina Alekseeva <alekseeva2311@icloud.com>
2021-01-07 13:41:29 +0700

    JSX

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,14 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+const value = 100;
+const flag = false;
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p style={{
+          color: 'red',
+          fontWeight: 'bold'
+        }}>
+          Hello World!
         </p>
+        {value}
+        <p>{value - 50}</p>
+        {flag && 'flag is true'}
+        {flag ? 'flag is true' : 'flag is false'}
+        {undefined}
+        {null}
+        {false}
+        {true}
         <a
           className="App-link"
           href="https://reactjs.org"
