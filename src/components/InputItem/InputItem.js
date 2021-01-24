de79e5915d620eb57090ee2d0dbc55080e0b8ac4
Angelina Alekseeva <alekseeva2311@icloud.com>
2021-01-24 19:00:59 +0700

    Material UI

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,5 +1,12 @@
 import React from 'react'
+import TextField from '@material-ui/core/TextField';
 
-const Input = () => (<input type="text" placeholder="что нужно сделать?"></input>);
+const Input = () => (
+  <TextField
+          id="filled-password-input"
+          label="Что нужно сделать?"
+          type="text"
+  />
+)
 
 export default Input;
--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -2,11 +2,14 @@ import React from 'react';
 import styles from './Item.module.css'
 import classnames from 'classnames';
 
-const Item = ({todoItems, isDone}) => (<span className={
-  classnames({
-    [styles.item]: true,
-    [styles.done]: isDone
-  })
-}>{todoItems}</span>);
+const Item = ({todoItems, isDone}) => (
+  <span className={
+    classnames({
+      [styles.item]: true,
+      [styles.done]: isDone
+    })
+  }>
+    {todoItems}
+  </span>);
 
 export default Item;
