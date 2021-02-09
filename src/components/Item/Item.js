b90b46c3326361981a3ec6e2a2195557e69035cb
Angelina Alekseeva <alekseeva2311@icloud.com>
2021-02-09 13:29:37 +0700

    методы жизненного цикла

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -3,29 +3,47 @@ import styles from './Item.module.css'
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 
-const Item = ({todoItems, isDone, onClickDone, id, onClickDelete}) => (
-  <div className={styles.wrapper}>
-    <label className={styles.label}>
-      <input type="checkbox" className={styles.checkbox} onClick={() => onClickDone(id)}/>
-      <span className={
-        classnames({
-          [styles.fake]: true,
-          [styles.fakeDone]: isDone
-        })
-      }></span>
-      <div className={styles.text}>
-        <span className={
-          classnames({
-            [styles.item]: true,
-            [styles.done]: isDone
-          })
-        }>
-          {todoItems}
-        </span>
-      </div>
-    </label>
-    <div className={styles.trash} onClick={() => onClickDelete(id)}></div>
-</div>)
+class Item extends React.Component {
+componentDidMount() {
+  console.log('componentDidMount');
+}
+
+componentDidUpdate() {
+  console.log('componentDidUpdate');
+}
+
+componentWillUnmount() {
+  console.log('componentWillUnmount');
+}
+
+  render() {
+    const {todoItems, isDone, onClickDone, id, onClickDelete} = this.props;
+
+    return (
+      <div className={styles.wrapper}>
+        <label className={styles.label}>
+          <input type="checkbox" className={styles.checkbox} onClick={() => onClickDone(id)}/>
+          <span className={
+            classnames({
+              [styles.fake]: true,
+              [styles.fakeDone]: isDone
+            })
+          }></span>
+          <div className={styles.text}>
+            <span className={
+              classnames({
+                [styles.item]: true,
+                [styles.done]: isDone
+              })
+            }>
+              {todoItems}
+            </span>
+          </div>
+        </label>
+        <div className={styles.trash} onClick={() => onClickDelete(id)}></div>
+    </div>);
+  }
+}
 
 Item.propTypes = {
   todoItems: PropTypes.string.isRequired,
